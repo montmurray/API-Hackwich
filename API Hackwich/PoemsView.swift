d535ddec79307672758aea39f1e6495d1dac44e4
montmurray <vinylscratch1103@gmail.com>
2025-02-27 17:18:33 -0600

    Setup PoemsView

---
+++ API Hackwich/PoemsView.swift
@@ -0,0 +1,18 @@
+//
+//  PoemsView.swift
+//  API Hackwich
+//
+//  Created by Tessa Murray on 2/27/25.
+//
+
+import SwiftUI
+
+struct PoemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PoemsView()
+}
