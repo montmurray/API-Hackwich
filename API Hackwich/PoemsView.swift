15f27db120b8f768d943ff3204934ede7c3ba3b4
montmurray <vinylscratch1103@gmail.com>
2025-02-27 17:22:42 -0600

    Re-linked the account, yay.

--- API Hackwich/PoemsView.swift
+++ API Hackwich/PoemsView.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct PoemsView: View {
+    @State private var poems = [Poem]()
+    @State private var showingAlert = false
+    let author: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(poems) { poem in
+            NavigationLink{
+                ScrollView {
+                    VStack {
+                        Text(poem.title)
+                            .font(.headline)
+                        Text("by \(author)").padding(.bottom)
+                        ForEach(poem.lines, id: \.self) { line in
+                            Text(line)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }
+                        Spacer()
+                    }
+                }
+            } label: {
+                Text(poem.title)
+            }
+        }
+        .navigationTitle("Poems by " + author)
+        .navigationBarTitleDisplayMode(.inline)
+        .task {
+            await getPoems()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the poems"), dismissButton: .default(Text("Ok")))
+        }
+    }
+    
+    func getPoems() async {
+        let query = "https://poetrydb.org/author/" + author + "/title, lines"
+        if let url = URL(string: query) {
+            if let (data, _) = try? await URLSession.shared.data(from: url) {
+                if let decodedResponse = try? JSONDecoder().decode([Poem].self, from: data) {
+                    poems = decodedResponse
+                    return
+                }
+            }
+        }
+        showingAlert = true
     }
 }
 
 #Preview {
-    PoemsView()
+    PoemsView(author: "Walt Whitman")
+}
+
+struct Poem: Identifiable, Codable {
+    let id = UUID()
+    let title: String
+    let lines: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case title, lines
+    }
 }
